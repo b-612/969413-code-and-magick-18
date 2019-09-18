522b474f04357f0884caa111f77f37bcc6cda6ff
b-612 <fromtheorion@yandex.ru>
2019-09-18 03:28:38 +0300

    Выполняет задание: 4. Учебный проект: нас орда

--- js/setup.js
+++ js/setup.js
@@ -1,2 +1,109 @@
 'use strict';
 
+var HOW_MANY_CHARACTERS = 4;
+var charactersParams = {
+  wizardNames: [
+    'Иван',
+    'Хуан Себастьян',
+    'Мария',
+    'Кристоф',
+    'Виктор',
+    'Юлия',
+    'Люпита',
+    'Вашингтон'
+  ],
+
+  wizardLastNames: [
+    'Фамилии',
+    'да Марья',
+    'Верон',
+    'Мирабелла',
+    'Вальц',
+    'Онопко',
+    'Топольницкая',
+    'Нионго',
+    'Ирвинг'
+  ],
+
+  coatColors: [
+    'rgb(101, 137, 164)',
+    'rgb(241, 43, 107)',
+    'rgb(146, 100, 161)',
+    'rgb(56, 159, 117)',
+    'rgb(215, 210, 55)',
+    'rgb(0, 0, 0)'
+  ],
+
+  eyesColors: [
+    'black',
+    'red',
+    'blue',
+    'yellow',
+    'green'
+  ]
+};
+
+var getRandomNumber = function (maxNumber) {
+  return Math.floor(Math.random() * (maxNumber + 1));
+};
+
+var showSettingsWindow = function () {
+  var playerSettings = document.querySelector('.setup');
+  playerSettings.classList.remove('hidden');
+};
+
+var makeCharacters = function (characterParams, howMany) {
+  var makeCharacter = function () {
+    var character = {
+      name: characterParams.wizardNames[getRandomNumber(characterParams.wizardNames.length - 1)] + ' ' + characterParams.wizardLastNames[getRandomNumber(characterParams.wizardLastNames.length - 1)],
+
+      coatColor: characterParams.coatColors[getRandomNumber(characterParams.coatColors.length - 1)],
+
+      eyesColor: characterParams.eyesColors[getRandomNumber(characterParams.eyesColors.length - 1)]
+    };
+
+    return character;
+  };
+
+  var characters = [];
+
+  for (var i = 0; i < howMany; i++) {
+    characters.push(makeCharacter());
+  }
+
+  return characters;
+};
+
+var showSimilarCharacters = function (characters) {
+  var similarCharacters = document.querySelector('.setup-similar');
+  var similarList = document.querySelector('.setup-similar-list');
+  var similarCharacterTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
+  var renderCharacter = function () {
+    var characterElement = similarCharacterTemplate.cloneNode(true);
+    var characterNameElement = characterElement.querySelector('.setup-similar-label');
+    var characterCoatElement = characterElement.querySelector('.wizard-coat');
+    var characterEyesElement = characterElement.querySelector('.wizard-eyes');
+
+    characterNameElement.textContent = characters[i].name;
+    characterCoatElement.style.fill = characters[i].coatColor;
+    characterEyesElement.style.fill = characters[i].eyesColor;
+
+    return characterElement;
+  };
+
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < characters.length; i++) {
+    fragment.appendChild(renderCharacter());
+  }
+
+  similarList.appendChild(fragment);
+  similarCharacters.classList.remove('hidden');
+};
+
+var showPlayerSettings = function (characterParams, howManyCharacters) {
+  showSettingsWindow();
+  showSimilarCharacters(makeCharacters(characterParams, howManyCharacters));
+};
+
+showPlayerSettings(charactersParams, HOW_MANY_CHARACTERS);
