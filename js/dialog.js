898a32304e1af6efc5d6650fa5242dc953b3967c
b-612 <fromtheorion@yandex.ru>
2019-10-03 20:01:08 +0300

    Рабочий вариант с вложенностью

--- js/dialog.js
+++ js/dialog.js
@@ -7,35 +7,12 @@
     dialogHandler.removeEventListener('click', onClickPreventDefault);
   };
 
-  var onMouseMove = function (isDragged, startCoordinates) {
-    return function (moveEvt) {
-      moveEvt.preventDefault();
-      isDragged = true;
-
-      var shift = {
-        x: startCoordinates.x - moveEvt.clientX,
-        y: startCoordinates.y - moveEvt.clientY
-      };
-
-      startCoordinates = {
-        x: moveEvt.clientX,
-        y: moveEvt.clientY
-      };
-
-      window.playerSettings.dialog.setAttribute('style', 'top: '
-        + (window.playerSettings.dialog.offsetTop - shift.y) + 'px; ' +
-        'left: ' + (window.playerSettings.dialog.offsetLeft - shift.x) + 'px');
-
-      return isDragged;
-    };
-  };
-
   var onMouseUp = function (moveResult) {
     return function (upEvt) {
       // debugger;
       upEvt.preventDefault();
 
-      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mousemove', moveResult);
       document.removeEventListener('mouseup', onMouseUp);
 
       if (moveResult) {
@@ -55,7 +32,28 @@
 
     var dragged = false;
 
-    document.addEventListener('mousemove', onMouseMove(dragged, startCoords));
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      dragged = true;
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      window.playerSettings.dialog.setAttribute('style', 'top: '
+        + (window.playerSettings.dialog.offsetTop - shift.y) + 'px; ' +
+        'left: ' + (window.playerSettings.dialog.offsetLeft - shift.x) + 'px');
+
+      return dragged;
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mouseup', onMouseUp(onMouseMove));
   };
 
